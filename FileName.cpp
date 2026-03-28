865a227a7828e9c890f6a80ec0ae1c9044baf980
xinzhu923 <13591002347@163.com>
2026-03-28 10:49:08 +0800

    九九乘法表C++代码

---
+++ FileName.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main() {
+	for (int i = 1; i <=9; i++) {
+		for (int j = 1; j <= 9; j++) {
+			cout << j << "*" << i << "=" << setw(2)<<j * i << "  ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
+
